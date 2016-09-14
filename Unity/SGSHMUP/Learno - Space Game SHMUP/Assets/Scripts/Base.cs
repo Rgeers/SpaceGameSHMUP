317cd9eecab5804112ec32856f50f6cb56e89182
Rgeers <rik.geersing@gmail.com>
2016-09-14 10:36:09 +0200

    Unity files

---
+++ Unity/SGSHMUP/Learno - Space Game SHMUP/Assets/Scripts/Base.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract class Base : MonoBehaviour {
+    protected Stats stats;
+
+    protected abstract void Attack() {
+        
+    }
+
+    protected abstract void Move() {
+
+    }
+
+    protected virtual void TakeDamage() {
+
+    }
+
+    protected virtual void OnDeath() {
+
+    }
+
+}
+
+private class Stats {
+    protected int hitPoints, movementSpeed, damage;
+
+    public Stats(int hitPoints, int movementSpeed, int damage) {
+        this.hitPoints = hitPoints;
+        this.movementSpeed = movementSpeed;
+        this.damage = damage;
+    }
+
+}
---
+++ Unity/SGSHMUP/Learno - Space Game SHMUP/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Base {
+    private int ownHitPoints, ownMovementSpeed, ownDamage;
+	void Start() {
+        stats = new Stats(ownHitPoints, ownMovementSpeed, ownDamage);
+	}
+
+}
